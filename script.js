6f562246e1b12834e5ebcff84ab9228758f29129
Martin Noguera <mnoguera.desarrollo@gmail.com>
2025-08-23 20:56:15 -0300

    se corrigio la funcion addRow1

--- script.js
+++ script.js
@@ -50,11 +50,11 @@
   function addRow(d = { qty: "", unit: "", desc: "", price: "" }) {
     const tr = document.createElement("tr");
     tr.innerHTML = `<td><input type="number" value="${d.qty}"></td>
-      <td><input type="text" value="${d.unit}"></td>
-      <td><input type="text" value="${d.desc}"></td>
-      <td><input type="number" value="${d.price}"></td>
-      <td class="subtotal">—</td>
-      <td><button>✕</button></td>`;
+    <td><input type="text" value="${d.unit}"></td>
+    <td><input type="text" value="${d.desc}"></td>
+    <td><input type="number" value="${d.price}"></td>
+    <td class="subtotal">—</td>
+    <td><button>✕</button></td>`;
     tbody.appendChild(tr);
     tr.querySelectorAll("input").forEach((i) =>
       i.addEventListener("input", recalc)
